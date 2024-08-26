27fdd6b4bc520301d743703135264a783733e3d5
FatherofElias <supra_3_8@hotmail.com>
2024-08-26 15:17:13 -0500

    update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # For Task 1 Database configuration
-
+# The password placeholder is not my actual db password.
 class Config:
     host = 'localhost'
     database = 'fitness_db'
     user = 'root'
-    password = 'Elias928'
+    password = 'placeholder'
